ae1b75bf2f21fdbcbc6ed2833b273fdf54e348a8
Daniel Gil <daniel.gil.bayo@gmail.com>
2014-04-25 16:32:10 +0200

    Fixed bug where reaching end of outputfile the global mutex would be left open and block the server

--- utils.c
+++ utils.c
@@ -57,6 +57,7 @@ int readline_outputfile(char *buffer, server_rec *s)
     rv = apr_file_open(&cfg->output_fd, cfg->outputfile, APR_FOPEN_READ|APR_FOPEN_XTHREAD|APR_FOPEN_BUFFERED, APR_FPROT_OS_DEFAULT, cfg->pool);
     if (rv != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, "Failed to open file %s", cfg->outputfile) ;
+        apr_global_mutex_unlock(cfg->shm_mutex);
         return rv;
     }
    
@@ -64,12 +65,16 @@ int readline_outputfile(char *buffer, server_rec *s)
     rv = apr_file_seek(cfg->output_fd, APR_SET, (apr_off_t *) &cfg->shm_memory->outputfile_offset);
     if (rv != APR_SUCCESS) {
         ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, "Failed to move file pointer to the end of %s", cfg->outputfile) ;
+        apr_file_close(cfg->output_fd);
+        apr_global_mutex_unlock(cfg->shm_mutex);
         return rv;
     }
 
     /* Read the line and advance the counter */
     rv = apr_file_gets(buffer, 256, cfg->output_fd);
     if (rv != APR_SUCCESS) {
+        apr_file_close(cfg->output_fd);
+        apr_global_mutex_unlock(cfg->shm_mutex);
         return rv;
     }
     cfg->shm_memory->outputfile_offset += strlen(buffer);
@@ -83,11 +88,10 @@ int readline_outputfile(char *buffer, server_rec *s)
     /* Close the outputfile*/
     rv = apr_file_close(cfg->output_fd);
     if (rv != APR_SUCCESS) {
-        ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, "Failed to open file %s", cfg->outputfile) ;
+        ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, "Failed to close file %s", cfg->outputfile) ;
+        mutex_rv = apr_global_mutex_unlock(cfg->shm_mutex);
         return rv;
     }    
-
-    //http://www.codemass.com/mod_shm_counter/downloads/mod_shm_counter.c
 
     mutex_rv = apr_global_mutex_unlock(cfg->shm_mutex);
     if (mutex_rv != APR_SUCCESS) {
